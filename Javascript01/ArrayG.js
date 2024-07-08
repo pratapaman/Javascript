ed572b03b1134f0091c5b5f77b4915c54a716a78
Aman Pratap <92034310+pratapaman@users.noreply.github.com>
2024-07-08 11:43:05 +0000

    config

---
+++ Javascript01/ArrayG.js
@@ -0,0 +1,19 @@
+// const arr = Array()
+// or 
+let arr = Array(1, 2, 3, 'ab')
+consle.log(arr)
+
+const arr1 = []
+console.log(arr1)
+
+const number = [0, 3.14, 'orange']
+console.log('Numbers:', number)
+
+const arr2 = ['aman', 250, true, {conuntry: 'Finland',city: 'Helsinki'}, 
+    {skills: ['HTML', 'CSS', 'JS', 'React', 'Python']}
+]
+console.log(arr2)
+ 
+
+
+
